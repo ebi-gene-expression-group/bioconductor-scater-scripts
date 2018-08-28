5579d0b5911d49340d5851ea2e4c539a82599263
Jonathan Manning <jmanning@ebi.ac.uk>
2018-08-28 10:58:27 +0100

    Tidying and typos

--- scater-calculate-qc-metrics.R
+++ scater-calculate-qc-metrics.R
@@ -13,35 +13,35 @@ option_list = list(
     action = "store",
     default = NA,
     type = 'character',
-    help = "singleCellExperiment object containing expression values and experimental information. Must have been appropriatelyprepared"
+    help = "singleCellExperiment object containing expression values and experimental information. Must have been appropriately prepared."
   ),
   make_option(
     c("-e", "--exprs-values"),
     action = "store",
     default = 'counts',
     type = 'character',
-    help= "character(1), indicating slot of the 'assays' of the 'object' should be used to define expression? Valid options are 'counts' [default; recommended],'tpm','fpkm' and 'logcounts', or anything else in the object added manually by the user."
+    help= "character(1), indicating slot of the 'assays' of the 'object' that should be used to define expression. Valid options are 'counts' [default; recommended],'tpm','fpkm' and 'logcounts', or anything else in the object added manually by the user."
   ),
   make_option(
     c("-f", "--feature-controls"),
     action = "store",
     default = NULL,
     type = 'character',
-    help = "file containing a list of the control files with one feature per line. A named list is created containing one or more vectors to identify feature controls (for example, ERCC spike-in genes, mitochondrial genes, etc)"
+    help = "file containing a list of the control files with one file per line. Each control file should have one feature (e.g. gene) per line. A named list is created (names derived from control file names) containing one or more vectors to identify feature controls (for example, ERCC spike-in genes, mitochondrial genes, etc)"
   ),
   make_option(
     c("-c", "--cell-controls"),
     action = "store",
     default = NULL,
     type = 'character',
-    help = "file to be used to derive a vector of cell (sample) names used to identify cell controls (for example, blank wells or bulk controls)."
+    help = "file (one cell per line) to be used to derive a vector of cell (sample) names used to identify cell controls (for example, blank wells or bulk controls)."
   ),
   make_option(
     c("-n", "--nmads"),
     action = "store",
     default = 5,
     type = 'numeric',
-    help = "numeric scalar giving the number of median absolute deviations to be used to flag potentially problematic cells based on total counts (total number of counts for the cell, or library size) and total_features (number of features with non-zero expression). For total_features, cells are flagged for filtering only if total_features is 'nmads' below the median"
+    help = "numeric scalar giving the number of median absolute deviations to be used to flag potentially problematic cells based on total counts (total number of counts for the cell, or library size) and total_features (number of features with non-zero expression). For total_features, cells are flagged for filtering only if total_features is 'nmads' below the median."
   ), 
   make_option(
     c("-p", "--pct-feature-controls-threshold"),
@@ -71,14 +71,17 @@ if ( ! file.exists(opt$input_object_file)){
 # Check feature_controls
 if (! is.null(opt$feature_controls) && opt$feature_controls != 'NULL'){
     feature_control_files <- readLines(opt$feature_controls)
-    # Check the files one-by-on
+    
+    # Check the files one-by-one
     for (fileName in feature_control_files){
         if (! file.exists(fileName)){
           stop((paste('Supplied feature_controls file', fileName, 'does not exist')))
         }
     }
+    
     # Read the list of files into a list of vectors
     feature_controls <- lapply(feature_control_files, readLines)
+    
     #Give the list names derived from the individual file names
     names(feature_controls) <- basename(feature_control_files)
 }else{
@@ -96,7 +99,7 @@ if (! is.null(opt$cell_controls) && opt$cell_controls != 'NULL'){
   cell_controls <- NULL
 }
 
-# Once arguments are satisfcatory, load Scater package
+# Once arguments are satisfactory, load Scater package
 suppressPackageStartupMessages(require(scater))
 
 # Input from serialized R object
--- scater-get-random-genes.R
+++ scater-get-random-genes.R
@@ -11,10 +11,10 @@ nspikeins <- as.numeric(cl[3])
 ngenes <- as.numeric(cl[4])
 
 if (! file.exists(input_object_file)){
-  stop(paste('File', input_object_file, "does not exist"))
+    stop(paste('File', input_object_file, "does not exist"))
 }
 
-# Read Seurat object
+# Read SingleCellExperiment object
 suppressPackageStartupMessages(require(scater))
 SingleCellExperiment <- readRDS(input_object_file)
 
@@ -23,6 +23,7 @@ set.seed(42)
 random_genes=list()
 for (spikein in 1:nspikeins){
 	random_genes[[spikein]] <- sample(rownames(SingleCellExperiment), ngenes)
+    
     # Write to file
     writeLines(con=paste(output_text_file, spikein, sep="_"),random_genes[[spikein]])
     write.table(paste(output_text_file, spikein, sep="_"), file=output_text_file, append=TRUE, quote=FALSE, col.names=FALSE, row.names=FALSE)
